925f4a38c0d06e2fca70357bc2544753fc154525
Taylor <taybart@gmail.com>
2018-02-27 11:44:31 -0700

    Changed channel structure

--- main.go
+++ main.go
@@ -10,76 +10,103 @@ import (
 
 var l logger.Logger
 
+// Struct for channels
+type GuessResult struct {
+	distance int
+	input    []byte
+}
+
 func main() {
 	var routines = flag.Int("r", 4, "Amount of go routines to start")
 	flag.Parse()
 
 	l = logger.NewLogger(logger.LEVELDEBUG)
 
+	// Variable for keeping track of the best result
 	bestDistance := 450
 
-	hdc := make(chan int)
-	input := make(chan []byte)
+	// Channels for returning results
+	resc := make(chan GuessResult)
 
 	l.Debug("Starting %d routines", *routines)
+	// Spin up guessers
 	for i := 0; i < *routines; i++ {
-		go Guess(hdc, input)
+		go Guess(resc)
 	}
 
 	l.Info("Hashin away...")
-	for distance := range hdc {
-		in := <-input
-		if distance < bestDistance {
-			l.Info("New best Distance %d", distance)
-			l.Info("Input: %v", hex.EncodeToString(in))
-			bestDistance = distance
+	// Wait for distance to be sent
+	for gr := range resc {
+		// Check if this guess was better
+		if gr.distance < bestDistance {
+			l.Info("New best Distance %d", gr.distance)
+			l.Info("Input: %v", hex.EncodeToString(gr.input))
+			bestDistance = gr.distance
 		}
 	}
 }
-func Guess(hdc chan int, input chan []byte) {
+func Guess(resc chan GuessResult) {
+	// Get target hash
 	rmh := RandellMunroeHash()
+	// Variable to keep track of distance
 	bestDistance := 450
+	// Create byte array for guessing
 	toHash := make([]byte, 32)
 	for {
+		// Throw random garbage in
 		rand.Read(toHash)
+		// Hash away
 		skHash := Hash(toHash)
+		// Calculate hamming distance
 		distance := Hamming(rmh, skHash)
 
+		// See if we beat the best
 		if distance < bestDistance {
 			l.Verbose("Hash: %v", toHash)
 			bestDistance = distance
-			hdc <- distance
-			input <- toHash
+			// Send off results
+			resc <- GuessResult{distance, toHash}
 		}
 	}
 }
 
 func Hash(toHash []byte) []byte {
+	// Start a new Hasher
 	sk := new(skein.Skein1024)
+	// Skein1024
 	sk.Init(1024)
+	// Update hash
 	sk.Update(toHash)
 	outputBuffer := make([]byte, 128)
+	// get result and return as []byte
 	sk.Final(outputBuffer)
 	return outputBuffer[:]
 }
 
 func Hamming(target, output []byte) int {
 	count := 0
+	// Range over byte arrays
 	for i, b1 := range target {
 		b2 := output[i]
+		// Check bytes with xor
 		for x := b1 ^ b2; x > 0; x >>= 1 {
+			// Check if bytes match
 			if int(x&1) == 1 {
+				// increment distance
 				count++
 			}
 		}
 	}
+	// If count is 0 we win!
 	if count == 0 {
 		return 1
 	}
+	// return Hamming distance
 	return count
 }
 
 func RandellMunroeHash() []byte {
+	// Funciton to keep hash so code is more compact
 	return []byte{0x5b, 0x4d, 0xa9, 0x5f, 0x5f, 0xa0, 0x82, 0x80,
 		0xfc, 0x98, 0x79, 0xdf, 0x44, 0xf4, 0x18, 0xc8, 0xf9, 0xf1,
 		0x2b, 0xa4, 0x24, 0xb7, 0x75, 0x7d, 0xe0, 0x2b, 0xbd, 0xfb,
